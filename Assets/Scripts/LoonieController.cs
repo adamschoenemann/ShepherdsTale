0470ca4bc1f89053fbc43084f4b992033357043b
MathiasKirkegaard <MathiasKirkegaard151090@gmail.com>
2013-11-14 13:17:45 +0100

    commit

---
+++ Assets/Scripts/LoonieController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoonieController : WolfController {
+	
+	protected override bool IsPlayerAudible()
+	{
+		bool result = base.IsPlayerAudible();
+		if(result)
+		{
+			if(playerController.IsSneaking())
+			{
+				return false;
+			}
+		}
+		return result;
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+		GameObject player 	= GameObject.FindGameObjectWithTag("Player");
+		GameObject staff	= GameObject.FindGameObjectWithTag("Staff");
+		if(other.gameObject == player || other.gameObject == staff)
+		{
+			state = Wolf.State.Alerted;
+			
+		}
+	}
+	
+	void Update()
+	{
+		if(state	
+	}
+	
+}
