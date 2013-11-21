8e36078fa5e449a2c027a4d6d9f69a58e9c25600
Thorbjørn Wolf <thorbjorn@helga.in>
2013-11-21 11:37:52 +0100

    Got a counter for measuring player performance, plus several gameplay fixes

--- Assets/Scripts/QTScripts/QTHandler.cs
+++ Assets/Scripts/QTScripts/QTHandler.cs
@@ -3,9 +3,7 @@ using System.Collections;
 
 /*
  * 	TODO 
- *	Make a time window where the user may hit the correct button - current implementation sux
- *	Make images for arrow keys / Decide what keys should be used		W, A, S, D
- *	React to user input
+ *	Make images for WASD keys in the form of arrows
  */
 
 public class QTHandler : MonoBehaviour {
@@ -13,44 +11,66 @@ public class QTHandler : MonoBehaviour {
 	public int nodeSize = 100;
 	public float nodesPerSecond = 1.0f;
 	public float inputPrecision = 0.166667f;
-	public TextAsset qteList;
+	public TextAsset quickTimeEventList;
 	public GUIStyle eventStyle;
 	public GUIStyle markerFieldStyle;
 
 	private int xCenter, yCenter;
 	private QTStream stream;
+	private bool keyPressed = false;
+	private int currentIndex = -1;
+	private int score = 0;
 
 	void Start () 
 	{
-		stream = new QTStream(qteList, nodesPerSecond, nodeSize);
-		xCenter = Screen.width/2;
+		stream = new QTStream(quickTimeEventList, nodesPerSecond, nodeSize);
+		xCenter = Screen.width/2 + (int)(nodeSize * inputPrecision); // Offset so that fields appear in the centre when they are in the middle of their selectability.
 		yCenter = Screen.height - (nodeSize/2 + 10);
 	}
-	
+
 	void Update () 
 	{
 		stream.Update();
 
 		float progress = stream.GetProgress();
-		progress -= (int)progress;
-		int index = (int)progress;
 
 
-		if(Input.GetKeyDown(stream.GetCurrentKeyCode()) && progress < 2 * inputPrecision)
+		if((int)progress > currentIndex)
 		{
-			// Users input is correct
-			Debug.Log("You pressed " + stream.GetCurrentKeyCode() + " just right!");
+			currentIndex = (int)progress;
+			keyPressed = false;
 		}
-		else if(Input.anyKey)
+		else if(keyPressed)
+		{
+			// Did user press a button when none was called for?
+			if(Input.anyKeyDown)
+			{
+				Debug.Log("You suck.");
+				score--;
+			}
+			
+			return; // Key has already been pressed
+		}
+
+		// Check whether user is inputting correct key at the right time
+		if(Input.GetKeyDown(stream.GetCurrentKeyCode()) && (progress - (int)progress) < 2 * inputPrecision)
+		{
+			Debug.Log("You pressed " + stream.GetCurrentKeyCode() + " just right!");
+			keyPressed = true;
+			score++;
+		}
+		else if(Input.anyKeyDown) // Check if user pressed some other key, or pressed any key at the wrong time
 		{
 			Debug.Log("You fail!");
+			keyPressed = true;
+			score--;
 		}
 	}
 
 	void OnGUI()
 	{
 		// Draw the stream
-		stream.Draw(xCenter + (int)(nodeSize * inputPrecision), yCenter);
+		stream.Draw(xCenter, yCenter);
 
 		// Draw marker box, indicating which events the user should pay attention to.
 		int sizeMod = 3;
@@ -60,6 +80,9 @@ public class QTHandler : MonoBehaviour {
 			sizeMod *= 2;
 		}
 
-		GUI.Box(new Rect(xCenter - nodeSize / 2 - sizeMod, yCenter - nodeSize / 2 - sizeMod, nodeSize + 2*sizeMod, nodeSize + 2*sizeMod), "", markerFieldStyle);
+		GUI.Box(new Rect(Screen.width/2 - nodeSize / 2 - sizeMod, 
+						 yCenter 		- nodeSize / 2 - sizeMod, 
+						 nodeSize + 2*sizeMod, 
+						 nodeSize + 2*sizeMod), score.ToString(), markerFieldStyle);
 	}
 }
