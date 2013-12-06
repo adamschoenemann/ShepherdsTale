c362ca1bffc8ea5146c078dbdf7db9f23223f899
micksi <mickneupart@gmail.com>
2013-12-06 12:44:25 +0100

    added wolf_main_scene

--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -10,6 +10,6 @@ public class Tags
 	public const string logger = "Logger";
 	public const string loonie = "Loonie";
 	public const string raceCourse = "RaceCourse";
-	public const string wayPoint = "Waypoint";
-	public const string checkPoint = "Checkpoint";
+	public const string wayPoint = "WayPoint";
+	public const string checkPoint = "CheckPoint";
 }
