347d1273682ebd899b9f8457e7d5361ea2acd24d
Thorbjørn Wolf <thorbjorn@helga.in>
2013-12-09 17:51:46 +0100

    Inserted "are you returning or not?" question in the very beginning, and logged it.

--- Assets/Scripts/PreQuestionnaire.cs
+++ Assets/Scripts/PreQuestionnaire.cs
@@ -1,31 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PreQuestionnaire : MonoBehaviour {
+public class PreQuestionnaire : Loggable
+{
 
 	public string questionnaireSceneName;
 
-	// Use this for initialization
-	void Start () {
-	
+	public override bool ShouldLogRoutinely()
+	{
+		return false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	protected override void BeforeEnqueueEntry(LogEntry entry)
+	{
+
 	}
+
+	void LogChoice(int val)
+	{
+		LogEntry entry = new LogEntry(this, "IsReturning")
+			.AddInt("returning", val);
+		EnqueueEntry(entry);
+			
+	}
+
 
 	void OnGUI()
 	{
 		GUI.skin.button.wordWrap = true;
 		
-		if(GUI.Button(new Rect((int)(Screen.width * 0.3),
-							   (int)(Screen.height * 0.3),
-							   (int)(Screen.width - 2 * (Screen.width * 0.3)),
-							   (int)(Screen.height - 2 * (Screen.height * 0.3))),
-								"Welcome! In order to play the game, please start by filling out this questionnaire. Click to begin!"))
+		GUI.Label(new Rect((int)(Screen.width * 0.3),
+							   (int)(Screen.height * 0.25),
+							   (int)(0.4f * Screen.width),
+							   (int)(0.25f * Screen.height)),
+								"Welcome! In order to play the game, please start by filling out the following questionnaire. Everything will be kept confidential. Click one of the buttons to begin!", "button");
+		if(GUI.Button(new Rect(
+								(int)(Screen.width * 0.3),
+							   	(int)(Screen.height * 0.5),
+							   	(int)(0.2f * Screen.width),
+							   	(int)(Screen.height - (Screen.height * 0.75))), "This is the first time I play this game."))
 		{
+		
+			LogChoice(0);
+
 			Application.LoadLevel(questionnaireSceneName);
+
+		}
+		if(GUI.Button(new Rect(
+								(int)(Screen.width * 0.5),
+							   	(int)(Screen.height * 0.5),
+							   	(int)(0.2f * Screen.width),
+							   	(int)(Screen.height - (Screen.height * 0.75))), "I am returning to get more."))
+		{
+			LogChoice(1);
+			Application.LoadLevel(questionnaireSceneName);
+
 		}
 	}
 }
